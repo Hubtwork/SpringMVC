efb7d1a7a5f51bace78e3e869858dd35b8b71d8d
Hubtwork <hubtwork@gmail.com>
2021-08-25 17:46:04 +0900

    feat: Spring MVC Project Init

---
+++ SpringMVCProject/src/main/kotlin/hubtwork/study/springmvcproject/SpringMvcProjectApplication.kt
@@ -0,0 +1,11 @@
+package hubtwork.study.springmvcproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvcProjectApplication>(*args)
+}
