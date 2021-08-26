918e7bae9bf441ded340b324aa1ffa3056a97274
Hubtwork <hubtwork@gmail.com>
2021-08-26 18:06:01 +0900

    feat: ItemRepository updated - require for comment, ItemController added

---
+++ ItemService/src/main/kotlin/hubtwork/study/itemservice/basic/BasicItemController.kt
@@ -0,0 +1,96 @@
+package hubtwork.study.itemservice.basic
+
+import hubtwork.study.itemservice.model.Item
+import hubtwork.study.itemservice.repository.ItemRepository
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.*
+import javax.annotation.PostConstruct
+
+
+@Controller
+@RequestMapping("/basic/items")
+class BasicItemController(
+    private val itemRepository: ItemRepository
+) {
+    /**
+     * Spring-Kotlin DI 방법
+     *
+     * 1. 필드
+     *  ## 스프링 프레임워크에 종속 ( Bean 을 컨테이너에서 직접 가져옴 )
+     *  > @Autowired
+     *  > private lateinit var itemRepository: ItemRepository
+     *
+     * 2. 단일 생성자 : 클래스에 생성자가 하나일 경우만 가능
+     *  ## 제일 안전함. 단일 책임원칙 및 Circular Dependency 파악에 용이
+     *  > ( private val itemRepository: ItemRepository )
+     *
+     * 3. SETTER
+     *  > @Autowired
+     *  > fun setRepository(itemRepository: ItemRepository)
+     *  > { this.itemRepository = itemRepository }
+     *
+     */
+
+    /**
+     * @PostConstruct
+     * - DI 주입 후 초기화 하고 싶을 떄 활용 ( lazy init )
+     */
+    @PostConstruct
+    fun init() {
+        itemRepository.save(Item(null, "testA", 10000, 10))
+        itemRepository.save(Item(null, "testB", 20000, 20))
+    }
+
+    @GetMapping
+    fun items(model: Model): String {
+        val items = itemRepository.findAll()
+        model.addAttribute("items", items)
+        return "basic/items"
+    }
+
+    /**
+     * V1. @RequestParam 이용
+     * V2. @ModelAttribute 이용
+     * V3. Model 생략
+     * V4. @ModelAttribute 생략
+     */
+    @PostMapping("/add")
+    fun addItemV1(
+        @RequestParam itemName: String,
+        @RequestParam price: Int,
+        @RequestParam quantity: Int,
+        model: Model
+    ) : String {
+        var item = Item(itemName = itemName, price = price, quantity = quantity)
+        itemRepository.save(item)
+        model.addAttribute("item", item)
+        print(itemRepository.findAll().size)
+        return "basic/item"
+    }
+
+    @PostMapping("/add-v2")
+    fun addItemV2(
+        @ModelAttribute("item") item: Item,
+        model: Model
+    ) : String {
+        itemRepository.save(item)
+        print(itemRepository.findAll().size)
+        return "basic/item"
+    }
+
+    @PostMapping("/add-v3")
+    fun addItemV3(@ModelAttribute("item") item: Item) : String {
+        itemRepository.save(item)
+        println(itemRepository.findAll().size)
+        return "basic/item"
+    }
+
+    @PostMapping("/add-v4")
+    fun addItemV4(item: Item) : String {
+        itemRepository.save(item)
+        print(itemRepository.findAll())
+        return "basic/item"
+    }
+
+}
