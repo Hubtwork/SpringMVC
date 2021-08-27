73d13ca7355410b31018a0610f816a1cd31b0aa6
Hubtwork <hubtwork@gmail.com>
2021-08-27 09:34:17 +0900

    Feat: ItemControllerService implemented

--- ItemService/src/main/kotlin/hubtwork/study/itemservice/basic/BasicItemController.kt
+++ ItemService/src/main/kotlin/hubtwork/study/itemservice/basic/BasicItemController.kt
@@ -5,6 +5,7 @@ import hubtwork.study.itemservice.repository.ItemRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import javax.annotation.PostConstruct
 
 
@@ -49,13 +50,28 @@ class BasicItemController(
         return "basic/items"
     }
 
+    @GetMapping("/{itemId}")
+    fun item(
+        @PathVariable itemId: Long,
+        model: Model
+    ): String {
+        val item = itemRepository.findById(itemId)
+        model.addAttribute("item", item)
+        return "basic/item"
+    }
+
+    @GetMapping("/add")
+    fun addForm() = "basic/addForm"
+
     /**
      * V1. @RequestParam 이용
      * V2. @ModelAttribute 이용
      * V3. Model 생략
      * V4. @ModelAttribute 생략
+     * V5. Redirect 이용 - 직접 String Resolver 에 변수를 대입할 경우 URL Encoding 아니어서 위험
+     * V6. RedirectAttributes 이용 - URL Encoding, PathVariable, QueryParameter 케어
      */
-    @PostMapping("/add")
+    @PostMapping("/add-v1")
     fun addItemV1(
         @RequestParam itemName: String,
         @RequestParam price: Int,
@@ -93,4 +109,51 @@ class BasicItemController(
         return "basic/item"
     }
 
+    @PostMapping("/add-v5")
+    fun addItemV5(item: Item) : String {
+        itemRepository.save(item)
+        return "redirect:/basic/items/${item.id}"
+    }
+
+    @PostMapping("/add")
+    fun addItemV6(
+        item: Item,
+        redirectAttributes: RedirectAttributes
+    ) : String {
+        val saved = itemRepository.save(item)
+        redirectAttributes.addAttribute("itemId", saved.id)
+        redirectAttributes.addAttribute("status", true)
+        /**
+         * RedirectAttributes
+         * - redirect UriPath 에 포함되어있는 Attribute 의 경우 PathVariable 로 매핑
+         * - 그 외의 Attribute 는 QueryParameter 로 처리
+         */
+        return "redirect:/basic/items/{itemId}"
+    }
+
+    /**
+     * 상품 수정 View
+     * 상품 수정 Redirection POST
+     *  - Spring 리다이렉트 view Resolver : 'redirect:' 키워드 사용, 이용된 @PathVariable 그대로 사용 가능
+     *
+     * HTML Form 전송 : GET, PUT 만 지원
+     */
+    @GetMapping("/{itemId}/edit")
+    fun editForm(
+        @PathVariable itemId: Long,
+        model: Model
+    ): String {
+        val item = itemRepository.findById(itemId)
+        model.addAttribute("item", item)
+        return "basic/editForm"
+    }
+
+    @PostMapping("/{itemId}/edit")
+    fun editItem(
+        @PathVariable itemId: Long,
+        @ModelAttribute item: Item
+    ): String {
+        itemRepository.save(item)
+        return "redirect:/basic/items/{itemid}"
+    }
 }
